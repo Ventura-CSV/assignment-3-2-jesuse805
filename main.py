3bfb31d70f13f17db0ed8399086362cba344e879
jesus <jesus_estrada5@my.vcccd.edu>
2024-06-26 17:07:22 -0700

    CS V11

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ def main():
     ########################################
     Code Your Program here
     """
+    # The "If" statements calulate the smallest number within the program
     if num1 <= num2 and num1 <= num3:
         minval = num1
     elif num2 <= num1 and num2 <= num3:
         minval = num2
     else:
         minval = num3
+        
+    
     
     
     """
