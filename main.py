33754d364af5fa26c5f00f9a93d9d77a4187e872
jesus <jesus_estrada5@my.vcccd.edu>
2024-06-26 18:54:56 -0700

    CS V11

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ def main():
     #Calculate Median
     if (num1 <= num2 and num1 >= num3) or (num1 >= num2 and num1 <= num3):
         median = num1 
+    elif (num2 <= num1 and num2 >= num3) or (num2 >= num1 and num2 <= num3):
         
     
     
